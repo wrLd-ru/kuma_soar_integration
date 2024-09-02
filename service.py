ec29b9386f35a6864d4a8da9e02d592cfb7f20e0
artem.s <78864780+wrLd-ru@users.noreply.github.com>
2024-09-02 12:33:44 +0300

    Update service.py

--- service.py
+++ service.py
@@ -20,18 +20,16 @@ async def incident(
         company: str = Form(...),
         level: str = Form(...)):
 
-    if company=='tenant_1':
+    if company==config['rvision']['tenant_1']['tenant_1_name']:
         RvisionAPI(
-        #host=config['rvision']['host'],
-        host='192.168.100.3',
+        host=config['rvision']['tenant_1']['host'],
         token=config['rvision']['tenant_1']['token'],
         protocol=config['rvision']['protocol'],
         verify=False)
 
-    if company=='tenant_2':
+    if company==config['rvision']['tenant_2']['tenant_2_name']:
         RvisionAPI(
-        #host=config['rvision']['host'],
-        host='192.168.100.4',
+        host=config['rvision']['tenant_2']['host'],
         token=config['rvision']['tenant_2']['token'],
         protocol=config['rvision']['protocol'],
         verify=False)
@@ -58,5 +56,4 @@ async def first_response():
 
 
 if __name__ == '__main__':
-#    uvicorn.run(app, host='192.168.100.42', port='5555')
     uvicorn.run(app, host=config['script']['host'], port=config['script']['port'])
